039c872ed8936d3c6821e1237717cd444e7a5fcf
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-03-23 21:22:21 +0545

    write function to display result

--- rps.js
+++ rps.js
@@ -9,27 +9,55 @@ function userPrompt(){
 
     return userInput;
 }
-userPrompt();
-console.log(userInput);
 
 // make computer pick any 3 values too in a new function called getComputerChoice
 function getComputerChoice(){
     // make array comp_val=["rock","paper","scissors"]
-    comp_val=["rock","paper","scissors"];
+    let comp_val=["rock","paper","scissors"];
     // randomize value from 1-3 name that value to x  -> Math.floor(Math.random() * 3);
     x=Math.floor(Math.random() * 3);
     // select any data from 'comp_val' using comp_val[x] into new variable called 'computerSelection'
     computerSelection=comp_val[x];
+    return computerSelection;
 }
-getComputerChoice();
-console.log(computerSelection);
+
 
 // send the user & comp value to a new function called playRound
+function playRound(user,comp){
+    // make a variable win_det=0
+    let win_det=0;
     // create logic to determine winner
-        // make a variable win_det=0
         // if st. to determine who won if won then win_det+=1
         // display win/lose and respective message of why defeated
+        if((user==="rock") && (comp==="paper")){
+            alert("Computer Wins! Paper wraps around Rock to immobilize it.");
+            win_det +=1;
+        }else if((user==="rock") && (comp==="scissors")){
+            alert("You Win! Rock smashes Scissors.");
+            win_det +=1;
+        }else if((user==="paper") && (comp==="rock")){
+            alert("You Win! Paper wraps around Rock to immobilize it.");
+            win_det +=1;
+        }else if((user==="paper") && (comp==="scissors")){
+            alert("Computer Wins! Scissors cuts Paper.");
+            win_det +=1;
+        }else if((user==="scissors") && (comp==="rock")){
+            alert("Computer Wins! Rock smashes Scissors.");
+            win_det +=1;
+        }else if((user==="scissors") && (comp==="paper")){
+            alert("You Win! Scissors cuts paper.");
+            win_det +=1;
+        }else{
+            alert("Draw");
+        }
+
+
+}
+
+let userChoice=userPrompt();
+let compChoice=getComputerChoice();
+playRound(userChoice,compChoice);
 
 // create new function called game and call 'playRound' 5 times
     // create a score track system with initial value being 0 -> score_player=0 & score_comp=0
-        // if the value 'win_det' is 1 then score_player +=1 else score_comp +=1 
+        // if the value 'win_det' is 1 then score_player +=1 else score_comp +=1
